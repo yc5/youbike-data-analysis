54588580231668db71ed4781c0f7f3d6d5a43dfd
yc5 <yc@xstar.me>
2024-03-22 05:23:18 +0800

    big: add control

--- analysis.py
+++ analysis.py
@@ -3,11 +3,15 @@ import glob
 import argparse
 
 
+def percentage_str(percentage, str_len):
+    return ("*" * int(percentage * str_len)).ljust(str_len, "-")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", help="station name", required=True)
+    parser.add_argument("-c", "--control", help="control station name")
     args = parser.parse_args()
-    station_name = args.name
 
     json_files = glob.glob(f"downloaded/taipei*.json")
     json_files.sort()
@@ -16,22 +20,30 @@ if __name__ == "__main__":
         try:
             with open(file, "r") as f:
                 data = json.load(f)
-                station = [x for x in data if station_name in x["sna"]]
+                station = [x for x in data if args.name in x["sna"]]
                 if len(station) == 0:
                     raise Exception("Station Name Not Found")
                 station = station[0]
-                print(station["sna"], end="")
-                print(",", end="")
+                if not args.control:
+                    print(station["sna"], end="")
+                    print(",", end="")
                 print(station["srcUpdateTime"], end="")
                 print(",", end="")
-                percentage = station["sbi"] / station["tot"]
-                percentage_len = 50
-                percentage_str = "*" * int(percentage * percentage_len)
-                print(percentage_str.ljust(percentage_len, "-"), end="")
-                print(",", end="")
-                print(station["tot"], end="")
-                print(",", end="")
-                print(station["sbi"], end="")
+                print(percentage_str(station["sbi"] / station["tot"], 50), end="")
+                if not args.control:
+                    print(",", end="")
+                    print(station["tot"], end="")
+                    print(",", end="")
+                    print(station["sbi"], end="")
+                else:
+                    station = [x for x in data if args.control in x["sna"]]
+                    if len(station) == 0:
+                        raise Exception("Station Name Not Found")
+                    station = station[0]
+                    print(
+                        "," + percentage_str(station["sbi"] / station["tot"], 50),
+                        end="",
+                    )
 
                 print()
         except FileNotFoundError:
